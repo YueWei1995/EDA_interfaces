e7601b825c4b43860e0663f38238c6cd3d6369f1
YueWei1995 <1119193288@qq.com>
2019-03-29 10:45:54 +0800

    Update Plot_corr_of_features_inPairs.py

--- Functions/Plot_corr_of_features_inPairs.py
+++ Functions/Plot_corr_of_features_inPairs.py
@@ -12,4 +12,13 @@ def Plot_corr_of_features_inPairs(df, cols, label):
   plt.title('Correlation between {} columns'.format(len(cols))
   plt.show()
   
-  
+# ##########################################################################
+# 与上一个函数类似，绘制一个dataframe中所有特征的两两间的相关度的热度图，相关度小于0.99的用0表示，大于0.99的用1表示，
+# 输入：一个dataframe
+# 输出：所有特征的两两间的相关性热度图
+def Plot_corr_of_features_inPairs_1(df)
+  corr = df.corr()
+  high_corr = (corr >= 0.99).astype('uint8')
+  plt.figure(figsize=(15,15))
+  sns.heatmap(high_corr, cmap='RdBu_r', annot=True, center=0.0)
+  plt.show()           
